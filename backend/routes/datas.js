a9f0fdac7299c382a12092a7f11fd45547f3698d
yeongin <pyi7628@gmail.com>
2019-08-22 18:32:14 +0900

    DB connect

--- backend/routes/datas.js
+++ backend/routes/datas.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var datas = require('../datas.json');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',   
+    password: 'duddlsdud1',
+    database: 'data'  
+  }); 
+  
+  connection.connect(function (err) {   
+    if (err) {     
+      console.error('mysql connection error');     
+      console.error(err);     
+      throw err;   
+    } 
+  });
 
 router.get('/', function (req, res, next) {
- res.send(datas)
+    connection.query('SELECT * FROM data.table', function(err, datas, fields) {
+        if(!err){
+          console.log(datas);
+          res.send(datas);
+        }
+        else
+          console.log('Error!!', err);
+      });
+ 
 });
 
 module.exports = router;
