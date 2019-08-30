75a5af992b56a9fb95cdaa91c97eca0d479af14e
Seongwon Jeong <rozencrants@ajou.ac.kr>
2019-08-30 21:10:57 +0900

    test

--- backend/routes/datas.js
+++ backend/routes/datas.js
@@ -6,8 +6,8 @@ var connection = mysql.createConnection({
     host: '127.0.0.1',
     port: 3306,
     user: 'root',   
-    password: 'duddlsdud1',
-    database: 'data'  
+    password: 'wjdtjd864@',
+    database: 'testdb'  
   }); 
   
   connection.connect(function (err) {   
---
+++ frontend/src/components/DataIndexPage.vue
@@ -0,0 +1,61 @@
+<!-- component -->
+
+<template>
+<div class="wrap">
+  {{returnMessage}}
+  <paginated-list :list-array="pageArray"/>
+  
+
+</div>
+
+  
+  
+</template>
+
+<script>
+import axios from 'axios'
+import PaginatedList from './PaginatedList'
+export default {
+    name: 'simple-pagination',
+    components:{
+      PaginatedList
+    },
+    props:{
+      message: { 
+        type: String, 
+        required: true,
+      } 
+    },
+    created () {
+      // 컴포넌트가 생성될 때, /api/movies에 요청을 보냅니다.   
+        this.$http.get('/api/datas')
+          .then((response) => {
+            this.pageArray = response.data;
+        })
+    },
+    data () {
+    return {
+      pageArray: []
+    }
+    },
+    computed:{
+      returnMessage(){
+        return this.message;
+      }
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Sunflower:300&display=swap&subset=korean');
+.table{
+  font-family: 'Sunflower', sans-serif;
+  width : 1000px;
+  height : 80px;
+  font-size : 20px;
+  border : 1px solid #BDBDBD;
+  padding : 10px;
+  background-color : white;
+  margin-left : 50px;
+}
+</style>
