81451dd21ee23cf15b063643924e9d681eababdc
Seongwon Jeong <rozencrants@ajou.ac.kr>
2019-08-30 17:08:30 +0900

    test1

--- frontend/src/components/DataIndexPage.vue
+++ frontend/src/components/DataIndexPage.vue
@@ -2,28 +2,28 @@
 
 <template>
 <div class="wrap">
-<table  cellspacing = '10'>
-  <tr v-for="d in datas">
-    <table class = "table">
-      <tr>
-        <span>{{d.title}}</span>
-      </tr>
-      <tr>
-        <span>{{d.subtitle}}</span>
-      </tr>
-    </table>
-  </tr>
-</table>
+  <input type = "text" v-model = "search" placeholder = "Search title.."/>
 
+  <div>
+    <div v-for="data in filteredList">
+      <a v-bind:href="data.id" target="_blank">
+        {{data.title}}</br>
+        {{data.subtitle}}
+      </a>
+    </div>
+  </div>
 </div>
+
   
   
 </template>
 
 <script>
+
 export default {
   created () {
-    // 컴포넌트가 생성될 때, /api/movies에 요청을 보냅니다.          
+    // 컴포넌트가 생성될 때, /api/movies에 요청을 보냅니다.        
+    this.search = ''  
     this.$http.get('/api/datas')
         .then((response) => {
           this.datas = response.data
@@ -31,11 +31,20 @@ export default {
   },
   data () {
     return {
+      
       datas: []
+    }
+  },
+  computed : {
+    filteredList(){
+      return this.datas.filter(data => {
+        return data.title.toLowerCase().includes(this.search.toLowerCase())
+      })
     }
   }
 }
 </script>
+
 <style>
 
 @import url('https://fonts.googleapis.com/css?family=Sunflower:300&display=swap&subset=korean');
