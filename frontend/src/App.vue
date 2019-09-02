359d37b1c7318845f7e6a82885147d8728f6c629
yeongin <pyi7628@gmail.com>
2019-09-02 18:18:34 +0900

    write checklist

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -30,38 +30,88 @@
           </div>
           지역 <br/>
           <div class = "detail">
-          <input type = "checkbox"> 서울특별시 <br/>
-          <input type = "checkbox"> 부산광역시 <br/>
-          <input type = "checkbox"> 광주광역시 <br/>
-          <input type = "checkbox"> 대구광역시 <br/>
-          <input type = "checkbox"> 인천광역시 <br/>
-          <input type = "checkbox"> 대전광역시 <br/>
-          <input type = "checkbox"> 울산광역시 <br/>
-          <input type = "checkbox"> 경기도 <br/>
-          <input type = "checkbox"> 충청북도 <br/>
-          <input type = "checkbox"> 충청남도 <br/>
-          <input type = "checkbox"> 강원도 <br/>
-          <input type = "checkbox"> 경상북도 <br/>
-          <input type = "checkbox"> 경상남도 <br/>
-          <input type = "checkbox"> 전라북도 <br/>
-          <input type = "checkbox"> 전라남도 <br/>
-          <input type = "checkbox"> 제주도 <br/>
+          <input type = "checkbox" id="서울" value="서울" v-model="checkedAge"> 
+          <label for="서울"> 서울특별시 </label>
+          <br/>
+          <input type = "checkbox" id="부산" value="부산" v-model="checkedAge">
+          <label for="부산"> 부산광역시 </label>
+          <br/>
+          <input type = "checkbox" id="광주" value="광주" v-model="checkedAge"> 
+          <label for ="광주"> 광주광역시 </label>
+          <br/>
+          <input type = "checkbox" id="대구" value="대구" v-model="checkedAge">
+          <label for ="대구"> 대구광역시 </label>
+          <br/>
+          <input type = "checkbox" id="인천" value="인천" v-model="checkedAge">
+          <label> 인천광역시 </label>
+          <br/>
+          <input type = "checkbox" id="대전" value="대전" v-model="checkedAge">
+          <label for = "대전"> 대전광역시 </label>
+          <br/>
+          <input type = "checkbox" id="울산" value="울산" v-model="checkedAge">
+          <label for="울산"> 울산광역시 </label>
+          <br/>
+          <input type = "checkbox" id="경기" value="경기" v-model="checkedAge">
+          <label for="경기"> 경기도 </label>
+          <br/>
+          <input type = "checkbox" id="충청북" value="충청북" v-model="checkedAge">
+          <label for="충청북"> 충청북도 </label>
+          <br/>
+          <input type = "checkbox" id="충청남" value="충청남" v-model="checkedAge">
+          <label for="충청남"> 충청남도 </label>
+          <br/>
+          <input type = "checkbox" id="강원" value="강원" v-model="checkedAge">
+          <label for="강원"> 강원도 </label>
+          <br/>
+          <input type = "checkbox" id="경상북" value="경상북" v-model="checkedAge">
+          <label for="경상북"> 경상북도 </label>
+          <br/>
+          <input type = "checkbox" id="경상남" value="경상남" v-model="checkedAge">
+          <label for="경상남"> 경상남도 </label>
+          <br/>
+          <input type = "checkbox" id="전라북" value="전라북" v-model="checkedAge">
+          <label for="전라북"> 전라북도 </label>
+          <br/>
+          <input type = "checkbox" id="전라남" value="전라남" v-model="checkedAge">
+          <label for="전라남"> 전라남도 </label>
+          <br/>
+          <input type = "checkbox" id="제주" value="제주" v-model="checkedAge">
+          <label for="제주"> 제주도 </label>
+          <br/>
           </div>
         
           목적 <br/>
           <div class = "detail">
-          <input type = "checkbox"> 취업 <br/>
-          <input type = "checkbox"> 금액 <br/>
-          <input type = "checkbox"> 주거 <br/>
-          <input type = "checkbox"> 교육 <br/>
-          <input type = "checkbox"> 의료 <br/>
-          <input type = "checkbox"> 문화 <br/>
+          <input type = "checkbox" id="취업" value="취업" v-model="checkedAge">
+          <label for="취업"> 취업 </label>
+          <br/>
+          <input type = "checkbox" id="금전" value="금전" v-model="checkedAge">
+          <lable for="금액"> 금전 </lable>
+          <br/>
+          <input type = "checkbox" id="주거" value="주거" v-model="checkedAge">
+          <label for="주거"> 주거 </label>
+          <br/>
+          <input type = "checkbox" id="교육" value="교육" v-model="checkedAge">
+          <label for="교육"> 교육 </label>
+          <br/>
+          <input type = "checkbox" id="의료" value="의료" v-model="checkedAge">
+          <label for="의료"> 의료 </label>
+          <br/>
+          <input type = "checkbox" id="문황" value="문화" v-model="checkedAge">
+          <label for="문화"> 문화 </label>
+          <br/>
           </div> 
           기타 <br/>
           <div class = "detail">
-          <input type = "checkbox"> 저소득층 <br/>
-          <input type = "checkbox"> 임산부 <br/>
-          <input type = "checkbox"> 장애인 <br/>
+          <input type = "checkbox" id="저소득층" value="저소득층" v-model="checkedAge">
+          <label for="저소득층"> 저소득층 </label>
+          <br/>
+          <input type = "checkbox" id="임산부" value="임산부" v-model="checkedAge">
+          <label for="임산부"> 임산부 </label>
+          <br/>
+          <input type = "checkbox" id="장애인" value="장애인" v-model="checkedAge">
+          <label for="장애인"> 장애인 </label>
+          <br/>
   <data-index-page :check-list="checkedAge"/>
   
  
